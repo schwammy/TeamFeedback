a01eb6c0a7b6e59eeff920435f20dff1d94fda6b
Andy <andy@ingenuitynow.net>
2021-05-03 13:20:49 -0400

    started the entry form.

---
+++ SchwammyStreams.TeamFeedback.BlazorServer/Data/FeedbackEntry.cs
@@ -0,0 +1,15 @@
+﻿
+
+using System.ComponentModel.DataAnnotations;
+
+namespace SchwammyStreams.TeamFeedback.BlazorServer.Data
+{
+    public class FeedbackEntry
+    {
+        [Required(ErrorMessage = "'Things Went Well' is required.")]
+        public string ThingsWentWell { get; set; }
+        [Required(ErrorMessage = "'Things went better' is required.")]
+        public string IsBetterThanLast { get; set; }
+
+    }
+}
