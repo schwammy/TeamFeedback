ae9678dc6dd148df696976bc4d490d06baf02f28
Andy <andy@ingenuitynow.net>
2021-05-04 12:51:14 -0400

    basic form works

--- SchwammyStreams.TeamFeedback.BlazorServer/Data/FeedbackEntry.cs
+++ SchwammyStreams.TeamFeedback.BlazorServer/Data/FeedbackEntry.cs
@@ -1,11 +1,16 @@
-﻿
-
+﻿using System;
 using System.ComponentModel.DataAnnotations;
 
 namespace SchwammyStreams.TeamFeedback.BlazorServer.Data
 {
     public class FeedbackEntry
     {
+        [Required(ErrorMessage = "'Team' is required.")]
+        public int? TeamId { get; set; }
+
+        [Required(ErrorMessage = "'Sprint End' is required.")]
+        public DateTime? SprintEnd { get; set; }
+
         [Required(ErrorMessage = "'Things Went Well' is required.")]
         public string ThingsWentWell { get; set; }
         [Required(ErrorMessage = "'Things went better' is required.")]
